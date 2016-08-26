980f6f9d93ee8ee4ce3485cde24c4e145baaa1dd
Rudie de Smit <Rudie de Smit>
2016-08-26 16:59:15 +0200

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,62 @@
+// script.js
+
+// create the module and name it scotchApp
+var app = angular.module('bijbelonderwijsAtHomeApp', ['ngRoute']);
+
+// route config
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+
+    .when('/home', {
+        templateUrl: './pages/home.html',
+        controller: 'homeController'
+    })
+    .when('/who', {
+        templateUrl: './pages/who.html',
+        controller: 'whoController'
+    })
+    .when('/education', {
+        templateUrl: './pages/education.html',
+        controller: 'educationController'
+    })
+    .when('/vision', {
+        templateUrl: './pages/vision.html',
+        controller: 'visionController'
+    })
+    .when('/subscribe', {
+        templateUrl: './pages/subscribe.html',
+        controller: 'subscribeController'
+    })
+    .when('/contact', {
+        templateUrl: './pages/contact.html',
+        controller: 'subscribeController'
+    })
+
+    .otherwise({
+        redirectTo: '/home'
+    })
+}]);
+
+// home controller
+app.controller('homeController', function ($scope) {
+});
+
+// who controller
+app.controller('whoController', function ($scope) {
+});
+
+// education controller
+app.controller('educationController', function ($scope) {
+});
+
+// vision controller
+app.controller('visionController', function ($scope) {
+});
+
+// subscribe controller
+app.controller('subscribeController', function ($scope) {
+});
+
+// contact controller
+app.controller('contactControler', function ($scope) {
+});
